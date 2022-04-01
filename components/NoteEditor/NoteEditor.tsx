10ca5fb8fe27f198ce6877d9ed1abb62032f83e6
acalebwilson24 <caleb@natureslaboratory.co.uk>
2022-04-01 19:53:22 +0100

    added explicit show editor state

--- components/NoteEditor/NoteEditor.tsx
+++ components/NoteEditor/NoteEditor.tsx
@@ -11,6 +11,7 @@ import { motion } from "framer-motion";
 const NoteEditor: React.FC<{ id?: number, isSuccess: { (id?: number): void }, isDeleted: { (): void } }> = ({ id, isSuccess, isDeleted }) => {
     const [noteID, setNoteID] = useState<number>()
     const [noteToEdit, setNote] = useState<InflatedNote>()
+    const [showEditor, setShowEditor] = useState(false);
     const mobile = useSelector((state: RootState) => state.mobile);
 
     const { data: note } = useGetNoteQuery(noteID, { skip: !noteID })
@@ -18,6 +19,7 @@ const NoteEditor: React.FC<{ id?: number, isSuccess: { (id?: number): void }, is
     useEffect(() => {
         if (note && noteID) {
             setNote(inflateNote(note))
+            setShowEditor(true);
         }
     }, [note, noteID])
 
@@ -29,18 +31,40 @@ const NoteEditor: React.FC<{ id?: number, isSuccess: { (id?: number): void }, is
 
     const backToMenuMobile = () => {
         setNoteID(undefined);
+        setShowEditor(false);
         setTimeout(() => {
             setNote(undefined);
         }, duration)
     }
 
+    const createNewNote = () => {
+        setNoteID(undefined);
+        setNote(getDefaultInflatedNote());
+        setShowEditor(true);
+    }
+
+    const animateState = () => {
+        console.log(mobile, showEditor);
+        if (mobile) {
+            if (showEditor) {
+                return "note"
+            } else {
+                return "menu"
+            }
+        } else {
+            return "desktop"
+        }
+    }
+
+    console.log(noteToEdit);
+
     return (
         <div
             className="h-full w-full absolute bg-white dark:bg-slate-900 flex overflow-hidden"
         >
-            <motion.div className="flex-grow md:relative grid grid-cols-[400px_auto] divide-x divide-slate-300 dark:divide-slate-600 shadow-lg shadow-slate-700/5" animate={mobile ? (noteID == undefined ? "menu" : "note") : "desktop"} >
+            <motion.div className="flex-grow md:relative grid grid-cols-[400px_auto] divide-x divide-slate-300 dark:divide-slate-600 shadow-lg shadow-slate-700/5" animate={animateState()} >
                 <motion.div className="absolute w-full h-full md:relative md:w-auto md:h-auto left-0 top-0 " variants={noteMenuVariants} transition={{ x: { type: "just", duration: duration/1000 } }}>
-                    <NoteEditorControls id={noteID} mobile={mobile ? true : false} setNoteID={setNoteID} createNewNote={() => { setNoteID(undefined); setNote(getDefaultInflatedNote()); }} />
+                    <NoteEditorControls id={noteID} mobile={mobile ? true : false} setNoteID={setNoteID} createNewNote={createNewNote} />
                 </motion.div>
                 <motion.div className="absolute overflow-auto flex flex-col w-full h-full md:relative md:w-auto md:h-auto left-0 top-0 bg-white py-4 md:py-0" variants={noteMainVariants} transition={{ x: { type: "just", duration: duration/1000 } }}  >
                     {mobile && <button className="mx-4 px-2 py-1 text-white rounded-md min-w-[80px] mb-2 bg-sky-600 self-start " onClick={backToMenuMobile}>Back</button>}
